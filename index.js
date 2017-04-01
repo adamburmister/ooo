91f176012ea974e98f0b34c3ddd99f2f52c57c07
Adam Burmister <adam.burmister@pinterest.com>
2017-04-01 09:20:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+var Botkit = require('botkit');
+var UserProfileUpdater = require('./profile_updater');
+var WebClient = require('@slack/client').WebClient;
+
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+  process.exit(1);
+}
+
+var DEFAULT_OOO_LOCATION = "OOO";
+
+var controller = Botkit.slackbot({
+  debug: true,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  json_file_store: './db',
+  scopes: [
+    'commands', 
+    'users.profile:read',
+    'users.profile:write'
+  ],
+});
+
+var token = process.env.SLACK_API_TOKEN || '';
+var web = new WebClient(token);
+
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+  controller.createWebhookEndpoints(controller.webserver);
+  controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
+});
+
+controller.on('slash_command', function (bot, message) {
+  switch (message.command) {
+    case "/ooo": // handle the `/ooo` slash command
+      if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+
+      // Deal with the Help case      
+      if (message.text === "help") {
+        bot.replyPrivate(message,
+        "I can update your username to include your Out Of Office status, " +
+        "for instance typing /ooo WFH would set your display name to \"Jonny Appleseed (WFH)\"");
+        return;
+      }
+
+      // Deal with the clear case
+      var isClearing = false;
+      if (message.text === "clear" || message.text === "reset" || message.text === "back") {
+        isClearing = true;
+      }
+
+      // Parse the location from the message
+      var location = message.text || DEFAULT_OOO_LOCATION;
+      console.log('message', message);
+
+      // Get the authed User's profile
+      web.users.profile.get({}, function(err, remoteUserData) {
+        console.log('remoteUserData', remoteUserData);
+        controller.storage.users.get(message.user_id, function(err, storedUserData) {
+          // If we haven't seen this user before we should save their profile as a backup
+          if (err || !storedUserData || !storedUserData.profile) {
+            storedUserData = Object.assign({id: message.user_id}, { 
+              profile: remoteUserData.profile, 
+              location: location 
+            });
+            controller.storage.users.save(storedUserData);
+            console.log('No stored user data', storedUserData);
+          }
+
+          // Otherwise this is a location update
+          UserProfileUpdater(
+            web,
+            message.user_id, 
+            { 
+              location: location,
+              isClearing: isClearing,
+              user: storedUserData 
+            }
+          ).then(function(updatedProfile) {
+            var reply;
+            if (isClearing) {
+              reply = "Welcome back! You're no longer marked as OOO, " + updatedProfile.real_name;
+              // clear our stored status for the user
+              controller.storage.users.delete(message.user_id);
+            } else {
+              reply = "You've been marked as OOO: " + updatedProfile.real_name;
+            }
+
+            bot.replyPrivate(message, reply);
+          });
+        });
+      });
+
+      break;
+    default:
+      bot.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+  }  
+});
---
+++ profile_updater.js
@@ -0,0 +1,45 @@
+
+/**
+ * @param {@slack/client} web
+ * @param {string} userId
+ * @param {object} oooConfig
+ * @param {string} oooConfig.location
+ * @param {bool} oooConfig.isClearing
+ * @param {object} oooConfig.user
+ * @param {object} oooConfig.user.profile
+ */
+module.exports = function(web, userId, oooConfig) {
+  return new Promise(function (resolve, reject) {
+    var lastName;
+
+    console.log('oooConfig', oooConfig);
+
+    try {
+      if (oooConfig.isClearing) {
+        lastName = oooConfig.user.profile.last_name;
+      } else {
+        lastName = oooConfig.user.profile.last_name + " (" + oooConfig.location + ")";
+      }
+    } catch(e) {
+      console.log(e);
+      reject('There was an error');
+    }
+
+    // Update their last name (+ OOO status) in their user profile
+    web.users.profile.set({
+      name: "last_name",
+      value: lastName
+    }, function(err, updatedUser) {
+      if (err) {
+        console.log('error setting user profile');
+        return reject("There was a problem updating your OOO status.");
+      }      
+
+      // TODO: Overlay a status icon on the User Avatar and call setPhoto
+      // to update it before resolving
+
+      console.log('setting user profile', updatedUser);
+      return resolve(updatedUser.profile);
+    });
+  });
+};
